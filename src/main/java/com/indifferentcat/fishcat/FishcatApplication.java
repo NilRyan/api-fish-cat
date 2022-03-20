0f5b265b75b53d9b4bfa94b45b5870cdbe44b851
Neil Ryan L <neilryanlipaod@gmail.com>
2022-03-20 18:33:39 +0800

    Initial commit

---
+++ src/main/java/com/indifferentcat/fishcat/FishcatApplication.java
@@ -0,0 +1,13 @@
+package com.indifferentcat.fishcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishcatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indifferentcat/fishcat/FishcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indifferentcat.fishcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
